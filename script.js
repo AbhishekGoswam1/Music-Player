d56c859e667f1d160159cb3575b957fde390ace3
Abhishek <goswamiabhi9729@gmail.com>
2025-07-07 16:37:40 +0530

    some minor fixes

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ closeBtn.addEventListener('click', () => {
     sidebar.classList.remove('active');
 });
 
+
+const artistBox = document.querySelector('.artist-box');
+const scrollLeftBtn = document.querySelector('.left-btn');
+const scrollRightBtn = document.querySelector('.right-btn');
+
+scrollLeftBtn.addEventListener('click', () => {
+    artistBox.scrollBy({
+        left: -150,
+        behavior: 'smooth'
+    });
+});
+
+scrollRightBtn.addEventListener('click', () => {
+    artistBox.scrollBy({
+        left: 150,
+        behavior: 'smooth'
+    });
+});
